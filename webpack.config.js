0c34cd24df8cd9eb16f08c36d876e4e2a6ed2893
Nikolay <shlyapnikov.nik@mail.ru>
2025-03-13 00:10:06 +0300

    Add eslint and prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,69 @@
-const path = require('path');
-const express = require('express');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const history = require('connect-history-api-fallback'); // добавляем connect-history-api-fallback
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
-module.exports = {
-    mode: 'development',
-    entry: './src/app/index.tsx',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
+// Получаем путь к текущей директории с использованием import.meta.url
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+
+export default {
+  mode: 'development',
+  entry: './src/app/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(scss|css)?$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-modules-typescript-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[local]__[hash:base64:5]',
+              },
             },
-            {
-                test: /\.(scss|css)?$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {loader: 'css-modules-typescript-loader'},
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: {
-                                localIdentName: '[local]__[hash:base64:5]'
-                            }
-                        },
-                    },
-                    {loader: 'sass-loader'}
-                ],
-                exclude: /node_modules/
-            }
+          },
+          { loader: 'sass-loader' },
         ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
+    hot: true,
+    port: 3000,
+    historyApiFallback: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        secure: false,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '',
         },
-        hot: true,
-        port: 3000,
-        historyApiFallback: true,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:4000',
-                secure: false,
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            }
-        },
+      },
     },
-    plugins: [
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({      
-            template: "./public/index.html"
-        })
-    ]
-};
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+}
