8a630bed8a4a3c3a5522f343d6cf387e343e2bb0
Nikolay <shlyapnikov.nik@mail.ru>
2025-03-18 00:36:27 +0300

    Пофиксить сборку

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,3 @@
-export const PHOTO_URL = 'http://localhost:8080'
-export const ADDRESS_URL = 'http://localhost:8081'
+export const PHOTO_URL = 'http://127.0.0.1:8080'
+export const ADDRESS_URL = 'http://127.0.0.1:8081'
 export const port = 3000
