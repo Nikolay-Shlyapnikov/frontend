8d1f5cd65a0d1dad41da06a2920fdd95dc40a828
Nikolay <shlyapnikov.nik@mail.ru>
2025-03-26 01:18:43 +0300

    Снова раздать стиля

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,3 @@
-export const PHOTO_URL = 'http://62.60.245.78:8080'
-export const ADDRESS_URL = 'http://62.60.245.78:8081'
+export const PHOTO_URL = 'http://127.0.0.1:8080'
+export const ADDRESS_URL = 'http://127.0.0.1:8081'
 export const port = 3000
